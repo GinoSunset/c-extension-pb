d6af25a62f81fa4ea40f7095a542583ab3143781
root <sunset>
2020-08-30 08:34:44 +0000

    test: add testing writing pb

--- tests/test_pb.py
+++ tests/test_pb.py
@@ -1,7 +1,10 @@
 import os
+import struct
 import unittest
+import gzip
 
 import pb
+
 MAGIC = 0xFFFFFFFF
 DEVICE_APPS_TYPE = 1
 TEST_FILE = "test.pb.gz"
@@ -16,14 +19,34 @@ class TestPB(unittest.TestCase):
         {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "apps": [1]},
     ]
 
+    magic_str = struct.pack('I', MAGIC)
+
     def tearDown(self):
         os.remove(TEST_FILE)
 
     def test_write(self):
         bytes_written = pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
         self.assertTrue(bytes_written > 0)
-        # check magic, type, etc.
+    
+    def test_file_write_has_magic(self):
+        pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
+        with gzip.open(TEST_FILE, 'rb') as f:
+            file_magic = f.read(4)
+            self.assertEqual(file_magic, self.magic_str)
 
+    def test_write_right_pb_id(self):
+        pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
+        with gzip.open(TEST_FILE, 'rb') as f:
+            file_magic = f.read(4)
+            pb_type, pb_len = struct.unpack('HH', f.read(4))
+            message = f.read(pb_len)
+
+            first_device_app = self.deviceapps[0]
+            id_device = first_device_app['device']['id']
+            _, id_from_file = struct.unpack(f"I{len(id_device)}s", message[:4+len(id_device)])
+            self.assertEqual(id_device, id_from_file.decode())
+                              
+    
     @unittest.skip("Optional problem")
     def test_read(self):
         pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
