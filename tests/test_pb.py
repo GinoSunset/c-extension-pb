97776d87a0bfd8a5df3cfe736a8dc3b0eff36e6c
root <sunset>
2020-08-30 11:12:42 +0000

    refactor: autoformating with black

--- tests/test_pb.py
+++ tests/test_pb.py
@@ -12,14 +12,31 @@ TEST_FILE = "test.pb.gz"
 
 class TestPB(unittest.TestCase):
     deviceapps = [
-        {"device": {"type": "idfa", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7c"},
-         "lat": 67.7835424444, "lon": -22.8044005471, "apps": [1, 2, 3, 4]},
-        {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "lat": 42, "lon": -42, "apps": [1, 2]},
-        {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "lat": 42, "lon": -42, "apps": []},
-        {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "apps": [1]},
+        {
+            "device": {"type": "idfa", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7c"},
+            "lat": 67.7835424444,
+            "lon": -22.8044005471,
+            "apps": [1, 2, 3, 4],
+        },
+        {
+            "device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"},
+            "lat": 42,
+            "lon": -42,
+            "apps": [1, 2],
+        },
+        {
+            "device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"},
+            "lat": 42,
+            "lon": -42,
+            "apps": [],
+        },
+        {
+            "device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"},
+            "apps": [1],
+        },
     ]
 
-    magic_str = struct.pack('I', MAGIC)
+    magic_str = struct.pack("I", MAGIC)
 
     def tearDown(self):
         os.remove(TEST_FILE)
@@ -27,26 +44,27 @@ class TestPB(unittest.TestCase):
     def test_write(self):
         bytes_written = pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
         self.assertTrue(bytes_written > 0)
-    
+
     def test_file_write_has_magic(self):
         pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
-        with gzip.open(TEST_FILE, 'rb') as f:
+        with gzip.open(TEST_FILE, "rb") as f:
             file_magic = f.read(4)
             self.assertEqual(file_magic, self.magic_str)
 
     def test_write_right_pb_id(self):
         pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
-        with gzip.open(TEST_FILE, 'rb') as f:
+        with gzip.open(TEST_FILE, "rb") as f:
             file_magic = f.read(4)
-            pb_type, pb_len = struct.unpack('HH', f.read(4))
+            pb_type, pb_len = struct.unpack("HH", f.read(4))
             message = f.read(pb_len)
 
             first_device_app = self.deviceapps[0]
-            id_device = first_device_app['device']['id']
-            _, id_from_file = struct.unpack(f"I{len(id_device)}s", message[:4+len(id_device)])
+            id_device = first_device_app["device"]["id"]
+            _, id_from_file = struct.unpack(
+                f"I{len(id_device)}s", message[: 4 + len(id_device)]
+            )
             self.assertEqual(id_device, id_from_file.decode())
-                              
-    
+
     @unittest.skip("Optional problem")
     def test_read(self):
         pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
